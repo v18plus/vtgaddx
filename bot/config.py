341d6e62f260faf11cee37e4e3280ebaf7bdb835
v18plus <86728159+v18plus@users.noreply.github.com>
2022-02-13 03:30:06 +0800

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) Shrimadhav U K
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+from dotenv import load_dotenv
+from bot.translation import Translation
+
+# apparently, no error appears even if the path does not exists
+load_dotenv("config.env")
+
+
+class Config:
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+    # required for running on Heroku
+    URL = os.environ.get("URL", "")
+    PORT = int(os.environ.get("PORT", 5000))
+    # Python3 ReQuests CHUNK SIZE
+    CHUNK_SIZE = 10280
+    # MyTelegram.org
+    # configurtion required while creating new application
+    APP_TITLE = os.environ.get("APP_TITLE", "vtgapibot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "vtgapibot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/vtgapibot")
+    # these platform informations were obtained
+    # on 27.01.2020 21:15:50 IST
+    APP_PLATFORM = [
+        "android",
+        "ios",
+        "wp",
+        "bb",
+        "desktop",
+        "web",
+        "ubp",
+        "other"
+    ]
+    # if any of the platform, does not work
+    # please reopen
+    # https://github.com/SpEcHiDe/MyTelegramOrgRoBot/issues/3
+    APP_DESCRIPTION = os.environ.get(
+        "APP_DESCRIPTION",
+        "created using https://telegram.dog/vtgapibot"
+    )
+    #
+    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @Graclex")
+    # the strings used in the different messages
+    # in the bot
+    START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
+    AFTER_RECVD_CODE_TEXT = os.environ.get(
+        "AFTER_RECVD_CODE_TEXT",
+        Translation.AFTER_RECVD_CODE_TEXT
+    )
+    BEFORE_SUCC_LOGIN = os.environ.get(
+        "BEFORE_SUCC_LOGIN",
+        Translation.BEFORE_SUCC_LOGIN
+    )
+    ERRED_PAGE = os.environ.get("ERRED_PAGE", Translation.ERRED_PAGE)
+    CANCELLED_MESG = os.environ.get(
+        "CANCELLED_MESG",
+        Translation.CANCELLED_MESG
+    )
+    IN_VALID_CODE_PVDED = os.environ.get(
+        "IN_VALID_CODE_PVDED",
+        Translation.IN_VALID_CODE_PVDED
+    )
+    IN_VALID_PHNO_PVDED = os.environ.get(
+        "IN_VALID_PHNO_PVDED",
+        Translation.IN_VALID_PHNO_PVDED
+    )
